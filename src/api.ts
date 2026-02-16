54d0b3f1a2fd55eaa5e0ba6f6d9bf17fe0772512
Wioleta Lip <wiolip@hotmail.com>
2026-02-16 18:54:49 +0100

    solution

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,20 @@
 import { MovieData } from './types/MovieData';
 import { ResponseError } from './types/ReponseError';
 
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=4d729f71';
+const BASE_URL = 'https://www.omdbapi.com/';
+const API_KEY = '4d729f71';
 
 export function getMovie(query: string): Promise<MovieData | ResponseError> {
-  return fetch(`${API_URL}&t=${query}`)
-    .then(res => res.json())
+  const url = `${BASE_URL}?apikey=${API_KEY}&t=${encodeURIComponent(query.trim())}`;
+
+  return fetch(url)
+    .then(res => {
+      if (!res.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      return res.json();
+    })
     .catch(() => ({
       Response: 'False',
       Error: 'unexpected error',
